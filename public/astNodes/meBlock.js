514bc830642ae0f349463cdd2225f56fbead6a79
unknown <michelleliu.ca@gmail.com>
2019-10-09 12:36:32 -0700

    MeBlock and ASTNode changes

--- public/astNodes/meBlock.js
+++ public/astNodes/meBlock.js
@@ -1,3 +1,21 @@
+import Formatting from "./format";
+
 export default class MeBlock {
     
+    parse() {
+        tokenizer.getAndCheckNext("About me");
+        text = tokenizer.getNext();
+        if (tokenizer.checkToken("Format")) {
+            format = new Formatting();
+        }
+        format.parse();
+    }
+
+    evaluate() {
+        this.fs.appendFileSync("../output.html", "<div>\n");
+        this.fs.appendFileSync("../output.html", "<h2>About Me</h2>\n");
+        this.fs.appendFileSync("../output.html", "<p>" + text + "</p>\n");
+        this.fs.appendFileSync("../output.html", "</div>\n");
+        format.evaluate();
+    }
 }
